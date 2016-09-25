f9b9f53c75f9c725a7f2d850e9aa3189d113a91b
matheusmig <matheus.mignoni@gmail.com>
2016-09-24 23:06:24 -0300

    Coding utf8

--- Python Package/Minimantics/CalculateSimilarity.py
+++ Python Package/Minimantics/CalculateSimilarity.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
 """ 
 Calculate Similarity Module
 """
--- Python Package/Minimantics/DataTypes.py
+++ Python Package/Minimantics/DataTypes.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
 """ 
 Data Types Module
 """
